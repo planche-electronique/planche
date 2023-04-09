ff65a0f4987e5b01a3fa7f791132317df91903ad
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-04-09 21:36:15 +0200

    remplissage du tableau "correct"

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    let tableau = document.getElementById("tableau");
+    var body_tableau = tableau.getElementsByTagName("tbody")[0];
+
+    tr = body_tableau.insertRow(0);
+    for (let i=0; i<12;i+=1) {
+        actual_th = tr.insertCell(0);
+        actual_th.textContent = i.toString();
+    }
+})
 
