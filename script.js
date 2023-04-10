66510ffbad5c47d4927cc8150f7f2a74742bd3ef
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-04-10 09:18:28 +0200

    show table once connected

--- script.js
+++ script.js
@@ -6,39 +6,41 @@ document.addEventListener('DOMContentLoaded', (event) => {
         let code_pilote = document.getElementById("champ_code_pilote").value;
         let mot_de_passe = document.getElementById("champ_mot_de_passe").value;
         let body = document.getElementById("body");
+        console.log(code_pilote);
+        console.log(mot_de_passe);
         /*requete*/
-    })
-    if (false) {
-        let body = document.getElementById("body");
+        let formulaire = document.getElementById("formulaire de connexion");
+        formulaire.remove();
+
         body.insertAdjacentHTML('beforeend',`<ul class="menu">
-            <li id="date_menu"><a><input type="date" id="date"></a></li>
-            <li class="text_menu"><a>Eplanche</a></li>
-            <li class="text_menu"><a>Affectation</a></li>
-        </ul>
-        <div>
-            
-        </div>
-        <table id="tableau">
-            <thead class="ligne_info">
-                <tr>
-                    <th>Ligne</th>
-                    <th>Code de décollage</th>
-                    <th>Avec</th>
-                    <th>Par</th>
-                    <th>Durée</th>
-                    <th>Immatriculation</th>
-                    <th>Code vol</th>
-                    <th>Heure de décollage</th>
-                    <th>Heure d'atterissage</th>
-                    <th> Durée du vol</th>
-                    <th>Commandant de bord ou instructeur</th>
-                    <th>Pilote 2 ou élève</th>
-                </tr>
-            </thead>
-            <tbody id="body_tableau"> 
-            </tbody>
-            
-        </table>`);
+                <li id="date_menu"><a><input type="date" id="date"></a></li>
+                <li class="text_menu"><a>Eplanche</a></li>
+                <li class="text_menu"><a>Affectation</a></li>
+            </ul>
+            <div>
+                
+            </div>
+            <table id="tableau">
+                <thead class="ligne_info">
+                    <tr>
+                        <th>Ligne</th>
+                        <th>Code de décollage</th>
+                        <th>Avec</th>
+                        <th>Par</th>
+                        <th>Durée</th>
+                        <th>Immatriculation</th>
+                        <th>Code vol</th>
+                        <th>Heure de décollage</th>
+                        <th>Heure d'atterissage</th>
+                        <th> Durée du vol</th>
+                        <th>Commandant de bord ou instructeur</th>
+                        <th>Pilote 2 ou élève</th>
+                    </tr>
+                </thead>
+                <tbody id="body_tableau"> 
+                </tbody>
+                
+            </table>`);
         let tableau = document.getElementById("tableau");
         var body_tableau = tableau.getElementsByTagName("tbody")[0];
         function nouvelle_ligne(body_tableau) {
@@ -48,9 +50,11 @@ document.addEventListener('DOMContentLoaded', (event) => {
                 actual_th.textContent = i.toString();
             }
         }
+
         for( let l = 0; l <40; l+=1) {
             nouvelle_ligne(body_tableau);
         }
-    }
+    })
+        
 })
 
