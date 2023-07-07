c0d179506fd4d5e1a16bf96171044cfe435cf613
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-07-07 15:08:11 +0200

    creation d'un fichier d'affectations pour les créér ou les supprimer

---
+++ js/affectations.js
@@ -0,0 +1,17 @@
+/*
+    Planche-électronique - projet de système d'enregistrement de vols en planeur pour clubs de vol à voile
+    
+    affectations.js
+        toutes les fonctions qui s'occupent de créer et supprimer des affectations
+    
+    Licensed under the MIT licence, provided 'as is' without any warranty.
+    Soumis à la licence MIT, fourni 'en l'état' sans aucune garantie.
+    Voir https://raw.githubusercontent.com/planche-electronique/planche/master/LICENSE pour la licence complète
+*/
+
+
+
+
+function creer_affectation() {
+  
+}
