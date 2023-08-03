8d1b2ebea80da31db5bb3453394c894a72f35cbe
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-08-03 15:04:12 +0200

    creation d'une fonctionde notification
    on cree une simple notification avec un div en haut a droite

--- js/elements_html.js
+++ js/elements_html.js
@@ -178,4 +178,19 @@ function bouton_mettre_heure(document, vol, champ, titre) {
 
 
 
+  function notification({top = 0, right = 0, className, html}) {
 
+    let notification = document.createElement('div');
+    notification.className = "notification";
+    if (className) {
+          notification.classList.add(className);
+        }
+
+    notification.style.top = top + 'px';
+    notification.style.right = right + 'px';
+
+    notification.innerHTML = html;
+    document.body.append(notification);
+
+    setTimeout(() => notification.remove(), 1500);
+}
